614c5ba8ad69fbb2a7c8c6a413b36528c4251a22
sohyun <parksohyan@gmail.com>
2021-07-23 17:59:26 +0900

    first commit

---
+++ src/main/java/com/baemin/server/ceo/CeoApplication.java
@@ -0,0 +1,13 @@
+package com.baemin.server.ceo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baemin/server/ceo/CeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baemin.server.ceo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
